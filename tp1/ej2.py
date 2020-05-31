3f46d4549908a7319455ab6dcb5066e0e08c9bbd
Andrés Carretero <andi_carretero@hotmail.com>
2020-05-31 02:41:47 +0200

    corregidos detalles de gráficos

--- tp1/ej2.py
+++ tp1/ej2.py
@@ -14,43 +14,35 @@ def generarSecuencia(secuencia, inicio, fin):
 
 # Creo una nueva secuencia con una semilla de 0.7
 secuencia2 = [0.7]
-generarSecuencia(secuencia2, 1, 10000)
+generarSecuencia(secuencia2, 1, 20000)
 
 # Para que de numeros entre 0 y 1, divido por su modulo
 secuencia2 = list(map(lambda n: n / modulo, secuencia2))
 
 
-# Ejercicio 2)
-# 2-a)Llamando al experimento A, el espacio muestral es S={2,3,4,5,6,7,8,9,10,11,12}
-# P(A=2)=1/12       P(A=3)=2/12       P(A=4)=3/12       P(A=5)=4/12       P(A=6)=5/12       P(A=7)=6/12
-# P(A=8)=5/12       P(A=9)=4/12       P(A=10)=3/12       P(A=11)=2/12       P(A=12)=1/12
-
-# 2-b)
-# Probabilidades de cada resultado respectivamente
-probabilidades = [1/36,2/36,3/36,4/36,5/36,6/36,5/36,4/36,3/36,2/36,1/36]
-
-# Cotas superiores de cada resultado posible,resultado de la suma de todas las probabilidades anteriores
-cota_superior = [0 for x in range(11)]
-for z in range(11):
-    for i in range(z+1):
-        cota_superior[z]+= probabilidades[i]
-
 # Asigna un resultado del evento según el valor de la variable "uniforme"
-def asignar_resultados(x):
-    resultado = 0
-    resultado_obtenido = False
-    i=0
-    while(resultado_obtenido==False):
-        if(x<=cota_superior[i]):
-            resultado_obtenido = True
-            resultado = i+2
-        i = i+1
-    return (resultado)
+def generar_valor_dado(x):
+    valor = 0
+    if(x<1/6):
+        valor=1
+    elif(x<2/6):
+        valor=2
+    elif(x<3/6):
+        valor=3
+    elif(x<4/6):
+        valor=4
+    elif(x<5/6):
+        valor=5
+    else:
+        valor=6
+    return (valor)
 
 f = []
 
-for i in secuencia2:
-    f.append(asignar_resultados((i)))
+for i in range(1,len(secuencia2),1):
+    dado_1 = generar_valor_dado(secuencia2[i])
+    dado_2 = generar_valor_dado(secuencia2[i-1])
+    f.append(dado_1+dado_2)
 
 
 # Histograma
