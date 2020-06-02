de464f7ff525243e41760111920c1edfd0c65aef
diego.costa <diego.costa@globallogic.com>
2020-06-02 02:31:38 -0300

    fix ejercicio 7

--- tp1/ej7.py
+++ tp1/ej7.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import random
+from math import e, pi, sqrt
 
-c = np.sqrt(2 * np.e / np.pi)
+c = sqrt(2 * e / pi)
 
 def generate_by_acceptance_rejection(n, mu, sigma):
   numbers = np.array([])
@@ -27,15 +28,6 @@ def generate_by_acceptance_rejection(n, mu, sigma):
 def prob_acumulada_empirica(numbers, x):
   elementos = list(filter(lambda element: element <= x, numbers))
   return len(elementos) / len(numbers)
-            
-
-def draw_histogram(numbers):
-  ax = plt.figure().add_subplot(1, 1, 1)
-  ax.hist(numbers, weights=np.zeros_like(numbers) +
-          1.0 / numbers.size, color="green", ec="black", alpha=0.5, bins=20)
-  ax.set_xlabel("Rango numérico")
-  ax.set_ylabel("Frecuencia relativa")
-
 
 def draw_acumulada_empirica(numbers):
   x = np.linspace(mu - 4 * sigma, mu + 4 * sigma)
@@ -47,35 +39,52 @@ def draw_norm_cdf(mu, sigma):
 
 
 alpha=0.01
-n = 100
+n = 10000
 mu = 25
 sigma = 2
 plus_deviations = []
 minus_deviations = []
 
 
-# Genero números aleatorios siguiendo una distribucion normal
+# Genero n números aleatorios siguiendo una distribucion N(mu, sigma)
 numbers = generate_by_acceptance_rejection(n, mu, sigma)
 
-#Ordeno las muestras de forma ascendente y busco los estadisticos
+# Efectua el test Komogorov-Smirnov y obtiene el p-value
+statistic, pvalue = stats.kstest(numbers, stats.norm(loc=mu, scale=sigma).cdf)
+
+print("Nivel de Significacion: {:.2f} ".format(alpha))
+print("p-valor: {:.2f} ".format(pvalue))
+
+# Comparo el p-value con el nivel de significancia deseado, y si es mayor, entonces
+# no hay evidencia para rechazar la hipotesis nula, y se acepta
+if alpha <= pvalue:
+  print("El test acepta la hipotesis nula.")
+else:
+  print("El test rechaza la hipótesis nula")
+
+
+# Alternativa: Ordena las muestras de manera ascendente y obtiene el estadistico
+# y lo compara con el valor limite obtenido de la tabla de Kolgomorov-Smirnov, 
+# Si el estadistico es <= valor limite, aceptamos la hipotesis
+'''
+#Ordeno las muestras de forma ascendente
 sorted_numbers = np.sort(numbers)
 
-###############
-#TODO: REVISAR POR QUE ESTA RECHAZANDO, QUEDAN MUY GRANDES LOS DESVIOS 
-##############
+#Obtengo las distancias entre la acumulada que evaluamos en la H0 y la empirica
+#de la muestra para asi hallar el estadistico
 for i in range(1,n):
-  plus_deviations.append(i/n - stats.norm.cdf(sorted_numbers[i]))
+  plus_deviations.append(i/n - stats.norm.cdf(sorted_numbers[i], mu, sigma))
 
 for i in range(1,n):
-  minus_deviations.append(stats.norm.cdf(sorted_numbers[i]) - (i - 1)/n)
+  minus_deviations.append(stats.norm.cdf(sorted_numbers[i], mu, sigma) - (i - 1)/n)
 
-kn_plus = np.sqrt(n) * np.max(plus_deviations)
-kn_minus = np.sqrt(n) * np.max(minus_deviations)
+kn_plus = sqrt(n) * max(plus_deviations)
+kn_minus = sqrt(n) * max(minus_deviations)
 
-estadistico = np.maximum(kn_plus, kn_minus)
+estadistico = max(kn_plus, kn_minus)
 
 #busco el limite en la tabla de kolmogorov-smirnof
-valor_limite = stats.ksone.ppf(1-alpha, n)
+valor_limite = sqrt(n) * stats.ksone.ppf(1-alpha, n)
 
 print("Limite superior: {:.2f} ".format(valor_limite))
 print("Estadistico: {:.2f} ".format(estadistico))
@@ -83,9 +92,11 @@ if estadistico <= valor_limite:
   print("El test acepta la hipotesis nula.")
 else:
   print("El test rechaza la hipótesis nula")
+'''  
 
 
-# Dibujo superpuestas la acumulada empirica y la acumulada esperada (normal)
+# Dibujo superpuestas la acumulada empirica y la acumulada esperada (N(25,2)),
+# donde no se aprecian distancias visibles en esta escala
 draw_acumulada_empirica(numbers)
 draw_norm_cdf(mu, sigma)
 plt.show()
