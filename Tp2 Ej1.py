d6ca787e60c5d7f2e766c38f2d3b6c5e06aa9e12
Leandro Percivati <l.percivati@gmail.com>
2020-06-24 21:37:09 -0300

    Update Tp2 Ej1.py
    Terminado a revisar alguna cosita

--- Tp2 Ej1.py
+++ Tp2 Ej1.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Si la distribucion es exponencial con media u=4hs entonces para una hora es de 0.25
+cantidadDeSolicitudes=100000
 
-cantidadDeSolicitudes=100
-
-def getTiemposDeEspera(tiemposDeEspera, duracionesDeProcesamientos, tiemposEntreArribos)
+def getTiemposDeEspera(duracionesDeProcesamientos, tiemposEntreArribos):
+    #La primera muestra no espera
     tiemposDeEspera=[0]
     for i in range(1, len(duracionesDeProcesamientos)):
         espera = tiemposDeEspera[i-1] + duracionesDeProcesamientos[i-1] - tiemposEntreArribos[i]
@@ -13,40 +12,75 @@ def getTiemposDeEspera(tiemposDeEspera, duracionesDeProcesamientos, tiemposEntre
             tiemposDeEspera.append(0)
         else:
             tiemposDeEspera.append(espera)
+    return tiemposDeEspera
+
+def getZeros(collection):
+    count = 0
+    for i in range(0, len(collection)):
+        if(collection[i]==0):
+            count = count +1
+    return count
+
+def getTiemposTotales(duracionesProcesamiento, tiemposDeEspera):
+    tiemposTotales = []
+    for i in range(len(duracionesProcesamiento)):
+        duracionTotal = duracionesProcesamiento[i] + tiemposDeEspera[i]
+        tiemposTotales.append(duracionTotal)
+    return tiemposTotales
 
 #genero llegadas de muestras
+#Si la distribucion es exponencial con media u=4hs entonces para una hora es de 1/4
+#exponencial es 1/lambda entonces 1/1/4 --> 4
 tiemposEntreArribos = np.random.exponential(4,cantidadDeSolicitudes)
 tiemposDeArriboDeMuestras=np.concatenate(([0],np.cumsum(tiemposEntreArribos)),axis=None) 
 
 #Separo tiempos de arribo de muestras en maquinas A1 y A2
-# probabilidades = np.random.rand(cantidadDeSolicitudes)
-# tiemposDeArriboDeMuestrasA1 =[0]
-# tiemposDeArriboDeMuestrasA2 =[0]
-# for i in range (cantidadDeSolicitudes):
-#     if(probabilidades[i] < 0.6):
-#         tiemposDeArriboDeMuestrasA1.append(tiemposDeArriboDeMuestras[i])
-#     else:
-#         tiemposDeArriboDeMuestrasA2.append(tiemposDeArriboDeMuestras[i])
+probabilidades = np.random.rand(cantidadDeSolicitudes)
+tiemposDeArriboDeMuestrasA1 =[0]
+tiemposDeArriboDeMuestrasA2 =[0]
+for i in range (cantidadDeSolicitudes):
+    if(probabilidades[i] < 0.6):
+        tiemposDeArriboDeMuestrasA1.append(tiemposDeArriboDeMuestras[i])
+    else:
+        tiemposDeArriboDeMuestrasA2.append(tiemposDeArriboDeMuestras[i])
 
-# tiemposEntreArribosA1=np.asarray(tiemposDeArriboDeMuestrasA1[1:])-np.asarray(tiemposDeArriboDeMuestrasA1[0:-1])
-# tiemposEntreArribosA2=np.asarray(tiemposDeArriboDeMuestrasA2[1:])-np.asarray(tiemposDeArriboDeMuestrasA2[0:-1]
+tiemposEntreArribosA1=np.asarray(tiemposDeArriboDeMuestrasA1[1:])-np.asarray(tiemposDeArriboDeMuestrasA1[0:-1])
+tiemposEntreArribosA2=np.asarray(tiemposDeArriboDeMuestrasA2[1:])-np.asarray(tiemposDeArriboDeMuestrasA2[0:-1])
 
-# duracionesDeProcesamientosMaquinaA1 = np.random.exponential(10/7,len(tiemposEntreArribosA1)) 
-# duracionesDeProcesamientosMaquinaA2 = np.random.exponential(1,len(tiemposEntreArribosA2))
+duracionesDeProcesamientosMaquinaA1 = np.random.exponential(10/7,len(tiemposEntreArribosA1)) 
+duracionesDeProcesamientosMaquinaA2 = np.random.exponential(1,len(tiemposEntreArribosA2))
 
-# tiemposDeEsperaA1=[]
-# tiemposDeEsperaA2=[]
+tiemposDeEsperaA1=[]
+tiemposDeEsperaA2=[]
                                                                              
-# getTiemposDeEspera(tiemposDeEsperaA1, duracionesDeProcesamientosMaquinaA1,tiemposEntreArribosA1)
-    
+tiemposDeEsperaA1 = getTiemposDeEspera(duracionesDeProcesamientosMaquinaA1,tiemposEntreArribosA1)
+tiemposDeEsperaA2 = getTiemposDeEspera(duracionesDeProcesamientosMaquinaA2,tiemposEntreArribosA2)                                                                             
+
+tiemposDeEsperaA =  tiemposDeEsperaA1 + tiemposDeEsperaA2
+
+print('Tiempo de espera promedio en maquinas A: ' + str(np.mean(tiemposDeEsperaA) * 60) + ' minutos')
+print("Fraccion de solicitudes que no esperaron:" + str(getZeros(tiemposDeEsperaA)/cantidadDeSolicitudes))
+
+duracionesTotalesA1 = getTiemposTotales(duracionesDeProcesamientosMaquinaA1, tiemposDeEsperaA1)
+duracionesTotalesA2 = getTiemposTotales(duracionesDeProcesamientosMaquinaA2, tiemposDeEsperaA2)
+duracionesTotalesA = duracionesTotalesA1 + duracionesTotalesA2
+
+promedioTiempoResolucionA = np.mean(duracionesTotalesA)
+                                                                             
 #genero tiempos de procesamientos de maquinaB 1/0.8 = 1.25
 duracionesDeProcesamientosMaquinaB = np.random.exponential(1.25,cantidadDeSolicitudes)
-    
-#La primera muestra no espera
-getTiemposDeEspera(tiemposDeEspera, duracionesDeProcesamientosMaquinaB, tiemposEntreArribos);
 
-print('Tiempo de espera promedio en maquinaB: ' + str(np.mean(tiemposDeEspera) * 60) + ' minutos')
+tiemposDeEsperaB = getTiemposDeEspera(duracionesDeProcesamientosMaquinaB, tiemposEntreArribos)
 
-# plt.step(muestras,range(len(muestras)),where= 'post' ,label='λ=4')
-# plt.legend()
-# plt.show()
+print('Tiempo de espera promedio en maquina B: ' + str(np.mean(tiemposDeEsperaB) * 60) + ' minutos')
+print("Fraccion de solicitudes que no esperaron:" + str(getZeros(tiemposDeEsperaB)/cantidadDeSolicitudes))
+
+duracionesTotalesB = getTiemposTotales(duracionesDeProcesamientosMaquinaB, tiemposDeEsperaB)
+promedioTiempoResolucionB = np.mean(duracionesTotalesB)
+
+print("Promedio tiempo A: " + str(promedioTiempoResolucionA))
+print("Promedio tiempo B: " + str(promedioTiempoResolucionB))
+if(promedioTiempoResolucionA  < promedioTiempoResolucionB * 0.5):
+    print("Conviene comprar Opcion A")
+else:
+    print("Conviene comprar Opcion B")   
